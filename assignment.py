f9e4db618586248bb9b995e946c3ef444ddf09e3
RyanK2023 <rk424318@ohio.edu>
2022-09-02 22:43:19 -0400

    updated output to have 7 tables, just need to fix output on laptop and add the other scatter plot

--- assignment.py
+++ assignment.py
@@ -48,7 +48,7 @@ df_sub2 = df.columns[df.columns.str.contains(pat = "KRAS")]
 #df.plot.scatter(x = 'Unnamed: 0', y = 'NumMutPerSample')
 #add a line for second scatter plot, will figure something out later 
 #print(col_sum)
-df_table = col_sum.to_frame('Total count')
+df_table = col_sum.to_frame('T')
 df_table = df_table.reset_index()
 df_table.rename(columns={'index':'Mutation List'}, inplace=True)
 
@@ -61,11 +61,11 @@ df_c = df[df["Unnamed: 0"].str.contains(pat = "NC") == False].reset_index()
 df_c = df_c.drop("index", axis = 1)
 #print(df_c)
 
-df_nc_sum = df_nc.sum(axis = 0).to_frame('Total nc')
+df_nc_sum = df_nc.sum(axis = 0).to_frame('NC')
 df_nc_sum = df_nc_sum.reset_index()
 df_nc_sum.rename(columns={'index':'Mutation List'}, inplace=True)
 
-df_c_sum = df_c.sum(axis = 0).to_frame('Total c')
+df_c_sum = df_c.sum(axis = 0).to_frame('C')
 df_c_sum = df_c_sum.reset_index()
 df_c_sum.rename(columns={'index':'Mutation List'}, inplace=True)
 
@@ -74,15 +74,36 @@ df_c_sum.rename(columns={'index':'Mutation List'}, inplace=True)
 df_table = df_table.merge(df_nc_sum, on = "Mutation List")
 df_table = df_table.merge(df_c_sum, on = "Mutation List")
 
-df_table['%C'] = df_table['Total c']/110
-df_table['%NC'] = df_table['Total nc']/120
+df_table['%C'] = df_table['C']/110
+df_table['%NC'] = df_table['NC']/120
 df_table['%C-%NC'] = df_table['%C'] - df_table['%NC']
 df_table['%C/%NC'] = df_table['%C'] / df_table['%NC']
 
-print(df_table.head(10).sort_values('Total count', ascending = False))
-print(df_table.head(10).sort_values('Total c', ascending = False))
-print(df_table.head(10).sort_values('Total nc', ascending = False))
-print(df_table.head(10).sort_values('%C', ascending = False))
-print(df_table.head(10).sort_values('%NC', ascending = False))
-print(df_table.head(10).sort_values('%C-%NC', ascending = False))
-print(df_table.head(10).sort_values('%C/%NC', ascending = False))
+df_t_sorted = df_table.sort_values('T', ascending = False).reset_index()
+df_t_sorted = df_t_sorted.drop("index", axis = 1)
+
+df_c_sorted = df_table.sort_values('C', ascending = False).reset_index()
+df_c_sorted = df_c_sorted.drop("index", axis = 1)
+
+df_nc_sorted = df_table.sort_values('NC', ascending = False).reset_index()
+df_nc_sorted = df_nc_sorted.drop("index", axis = 1)
+
+df_percentc_sorted = df_table.sort_values('%C', ascending = False).reset_index()
+df_percentc_sorted = df_percentc_sorted.drop("index", axis = 1)
+
+df_percentnc_sorted = df_table.sort_values('%NC', ascending = False).reset_index()
+df_percentnc_sorted = df_percentnc_sorted.drop("index", axis = 1)
+
+df_sub_sorted = df_table.sort_values('%C-%NC', ascending = False).reset_index()
+df_sub_sorted = df_sub_sorted.drop("index", axis = 1)
+
+df_div_sorted = df_table.sort_values('%C/%NC', ascending = False).reset_index()
+df_div_sorted = df_div_sorted.drop("index", axis = 1)
+
+print(df_t_sorted.head(10))
+print(df_c_sorted.head(10))
+print(df_nc_sorted.head(10))
+print(df_percentc_sorted.head(10))
+print(df_percentnc_sorted.head(10))
+print(df_sub_sorted.head(10))
+print(df_div_sorted.head(10))
